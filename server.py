feee01f511be02062dbc573021f6627355b323d0
KeshavMurthyK <kmurthyk@deloitte.com>
2023-06-28 13:39:36 +0530

    Added A new function

--- server.py
+++ server.py
@@ -2,9 +2,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
+items = [{
+        'name': 'Table',
+        'price': 5000
+    },
+    {
+        'name': 'Chair',
+        'price': 3000
+    }
+]
+
 @app.route('/')
 def index():
     return 'Deploying Flask Application using AWS ECR'
+
+
+@app.route('/items')
+def fetch_items():
+    return jsonify(items=items)
         
 if __name__ == "__main__":
     app.run()
